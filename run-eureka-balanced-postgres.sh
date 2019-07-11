af03735fd3386ee1d95ee47c038561f83576493e
Alberto <Alberto@Alberto-PC>
2019-07-11 22:20:29 +0200

    docker-compose-balanced-postgres.yml and sh ready!

---
+++ run-eureka-balanced-postgres.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# this file clone all projects to local folder and execute
+
+git clone https://github.com/albertoolivan/eureka-dijkstra.git
+
+git clone https://github.com/albertoolivan/server-registry.git
+
+git clone https://github.com/albertoolivan/rest-dto.git
+
+git clone https://github.com/albertoolivan/rest-producer.git
+
+git clone https://github.com/albertoolivan/rest-consumer.git
+
+mvn install -f eureka-dijkstra/pom.xml
+
+docker-compose -f eureka-dijkstra/docker-compose-balanced-postgres.yml up
